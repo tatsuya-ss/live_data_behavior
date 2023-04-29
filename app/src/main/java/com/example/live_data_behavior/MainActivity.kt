82132cf0be129b9657c5a6ec858899fe80377357
Tatsuya Sakamoto <tatsuya.sakamoto@jmty.jp>
2023-04-29 13:40:29 +0900

    ループ処理の検証

--- app/src/main/java/com/example/live_data_behavior/MainActivity.kt
+++ app/src/main/java/com/example/live_data_behavior/MainActivity.kt
@@ -19,28 +19,6 @@ class MainActivity : AppCompatActivity() {
         observedLiveData()
     }
 
-    override fun onStart() {
-        super.onStart()
-        Log.d("Tatsuya ٩( ᐛ )و", "onStart: ")
-    }
-
-    override fun onPause() {
-        Log.d("Tatsuya ٩( ᐛ )و", "onPause: ")
-        viewModel.onPause()
-        super.onPause()
-    }
-
-    override fun onStop() {
-        Log.d("Tatsuya ٩( ᐛ )و", "onStop: ")
-        viewModel.onStop()
-        super.onStop()
-    }
-
-    override fun onDestroy() {
-        Log.d("Tatsuya ٩( ᐛ )و", "onDestroy: ")
-        super.onDestroy()
-    }
-
     private fun observedLiveData() {
         viewModel.data.observe(this) {
             Log.d("Tatsuya ٩( ᐛ )و", "observedLiveData: $it")
--- app/src/main/java/com/example/live_data_behavior/MainViewModel.kt
+++ app/src/main/java/com/example/live_data_behavior/MainViewModel.kt
@@ -10,17 +10,8 @@ class MainViewModel: ViewModel() {
     val data: LiveData<Int> = _data
 
     fun onCreate() {
-        _data.value = 1
-        Log.d("Tatsuya ٩( ᐛ )و", "onCreate: ViewModelで　_data.value = 1　を実行")
-    }
-
-    fun onPause() {
-        _data.value = 2
-        Log.d("Tatsuya ٩( ᐛ )و", "onPause: ViewModelで　_data.value = 2　を実行")
-    }
-
-    fun onStop() {
-        _data.value = 3
-        Log.d("Tatsuya ٩( ᐛ )و", "onStop: ViewModelで　_data.value = 3　を実行")
+        for (number in 1..100) {
+            _data.value = number
+        }
     }
 }
